e2f22da9d7e865ca7c5fe28216bca9ca5ea522fd
Ryan Osgood <ryan@maybern.com>
2026-03-13 19:14:30 -0400

    Re-add MIT license field

--- Formula/td.rb
+++ Formula/td.rb
@@ -3,6 +3,8 @@ class Td < Formula
   homepage "https://github.com/rosgoo/td"
   url "https://github.com/rosgoo/td/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "fd7ada0eefa63f07789438ad3c441dac0ac9f3bfee431dd629746f0ef3d7747c"
+  license "MIT"
+
   depends_on "jq"
   depends_on "fzf"
   depends_on "gum"
